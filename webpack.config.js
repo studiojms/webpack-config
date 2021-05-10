314f0e87f61ade2f8a682ba95153a7919d59f33d
jefferson.souza <jefferson.souza@synchro.com.br>
2021-05-10 13:14:30 -0300

    importing css modules from npm dependency

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: './app/src/js/app.js',
@@ -9,19 +8,19 @@ module.exports = {
     path: path.resolve(__dirname, 'app/dist'),
     clean: true,
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: './app/src/app.html',
       filename: 'app.html',
       hash: true,
-    }),
-    new CopyWebpackPlugin({
-      patterns: [
-        {
-          from: './app/src/css',
-          to: 'css',
-        },
-      ],
     }),
   ],
 };
